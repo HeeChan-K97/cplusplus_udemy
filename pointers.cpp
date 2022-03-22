8418c47578f00bf47955fcd806d5f3938473a2f3
HeeChan-K97 <heechan6431@gmail.com>
2022-03-23 02:42:06 +1100

    Pointers Added

---
+++ pointers.cpp
@@ -0,0 +1,5 @@
+//! POINTERS: declaring pointers
+//Always initialize pointers, to point 'nowhere'
+//Uninitialized pointers contain garbage data and can point 'anywhere'
+//Initializing to zero or nullptr represents address zero
+// 만약 내가 포인터를 변수나 함수에 초기화 하지 않을 땐, 반드시 nullptr로 초기화 해주어야한다.
