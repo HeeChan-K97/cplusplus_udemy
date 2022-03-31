e1571ca80d8a847e066c50625861074d5310d762
HeeChan-K97 <heechan6431@gmail.com>
2022-04-01 01:04:49 +1100

    01.April

---
+++ Inheritance/inheritance.cpp
@@ -0,0 +1,19 @@
+Inheritance
+
+Terminology
+* Inheritance
+    Process of creating new classes from existing classes
+    Reuse mechanism
+* Single Inheritance
+    A new class is created from another 'single' class
+* Multiple Inheritance
+    A new class is created from two or more other classes  
+
+Base class = parent class, super class
+    The class being extended or inherited from
+
+Derived class = child class, sub class
+    The class being created from the Base class
+    Will inherit attributes and operations from Base class      
+
+---------INHERITANCE IS TRANSITIVE---------
