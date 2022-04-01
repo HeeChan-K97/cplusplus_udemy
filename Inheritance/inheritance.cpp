9a4a39d6b3417163fb564be1c047f9d148b25d36
HeeChan-K97 <heechan6431@gmail.com>
2022-04-01 14:54:40 +1100

    April.01

--- Inheritance/inheritance.cpp
+++ Inheritance/inheritance.cpp
@@ -17,3 +17,49 @@ Derived class = child class, sub class
     Will inherit attributes and operations from Base class      
 
 ---------INHERITANCE IS TRANSITIVE---------
+------------------------------------------------------------------------------------------
+Inheritance and Composition
+"is a" relationship VS "has a" relationship
+------------------------------------------------------------------------------------------
+Deriving classes from existing classes;
+
+1. C++ derivation syntax
+
+class Account {
+    // Account class members. . .
+};
+
+class Savings_Account : public Account {
+    //Savings_Account class members. . .
+};
+
+//Savings_Account is a Account.
+2. C++ creating objects
+
+*   Account account {};
+    Account *p_account = new Account();
+    account.deposit(1000.00);
+    p_account->withdraw(200.0);
+
+    delete p_account;
+
+*   Savings_Account sav_account {};
+    Savings_Account *p_sav_account = new Savings_Account();
+    sav_account.deposit(1000.0);
+    p_sav_account->withdraw(200.0);
+
+    delete p_sav_account;
+------------------------------------------------------------------------------------------
+
+Access specifier, Protected:
+
+    * Accessible from the Base class itself
+    * Accessibile from classes Derived from Base
+    * Not accessible by objects of Base or Derived
+
+*** members which are protected in the Base class is also become a protected members of a Derived class
+*** private members of the Base class is always private thata derived class can never get access
+*** private inheritance makes the members of the derived class private
+어떤 base class의 private 멤버의 값을 바꾸고 싶거나 정의하고 싶을때는 반드시 Base class에 주어진 getter and setter functions들을 통해서만 변경이 가능하다.
+즉, private를 base class가 아닌 다른 클래스에서 바꾸고 싶다면 그에 맞는 public 함수가 필요하다.
+
